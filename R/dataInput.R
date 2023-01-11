71619699b6015e6c765e61dd7197d85a8d0809ab
CarlijnB <cbogaardt@gmail.com>
2023-01-11 15:10:12 +0000

    Added components to extract & reformat input data

--- R/dataInput.R
+++ R/dataInput.R
@@ -1,13 +1,22 @@
-# Module UI function
+library(rlang)
+
+###################################
+### Data Input - User interface ###
+###################################
+
 dataInputUI <- function(id) {
-  # `NS(id)` returns a namespace function, which was save as `ns` and will
-  # invoke later.
-  ns <- NS(id)
+  ns <- NS(id) # `NS(id)` returns a namespace function
   tagList(
+
+# Data file upload --------------------------------------------------------
+
     h3("Upload", id, "data file"),
     fileInput(ns("data_file"), label = NULL,
               accept = c("text/csv", "text/comma-separated-values",
                          "text/plain", ".csv")),
+
+# Configurations ----------------------------------------------------------
+
     h3("Configurations"),
     fluidRow(
       column(4,
@@ -22,39 +31,35 @@ dataInputUI <- function(id) {
       column(4,
              style = "border-left: 1px solid",
              h4("File and data options"),
-             textInput(ns("encoding"), "Encoding",
-                       value = "UTF-8"),
-             textInput(ns("separator"), "Separator",
-                       value = ","),
-             textInput(ns("decimal"), "Decimal mark",
-                       value = "."),
+             textInput(ns("encoding"), "Encoding", value = "UTF-8"),
+             textInput(ns("separator"), "Separator", value = ","),
+             textInput(ns("decimal"), "Decimal mark", value = "."),
              create_data_options(id, ns)),
       column(4,
              splitLayout(
                h4("Data fields (column headers)"),
-               actionButton(ns("read_headers"),
-                            "Populate*"),
+               actionButton(ns("read_headers"), "Populate*"),
                cellWidths = c("67%","33%")),
              p("*Requires uploaded data file and completed file options"),
              create_data_fields(id, ns))
     ),
-    fluidRow(tableOutput(ns("datatable")))
 
-    #actionButton(ns("read_file"), "Read file",
-    #             width = "100%"),
+# Extract & reformat data -------------------------------------------------
 
-    #conditionalPanel(
-    #  condition = "input.read_file > 0",
-    #  actionButton(ns("show_head"), "Show first 3 rows",
-    #               width = "49.5%"),
-    #  actionButton(ns("show_full"), "Show full table",
-    #               width = "49.5%"),
-    #  #selectInput(),
-    #  ns = ns)
-    #conditionalPanel()
+    actionButton(ns("extract"), "Extract and reformat relevant columns",
+                 width = "100%"),
+    br(),
+    actionButton(ns("view_extracted_data"), "View extracted data",
+                 width = "100%"),
+    dataTableOutput(ns("datatable"))
+
 
   )
 }
+
+#################################
+### Data Input - Server logic ###
+#################################
 
 dataInputServer <- function(id, stringsAsFactors = FALSE) {
   moduleServer(
@@ -69,14 +74,16 @@ dataInputServer <- function(id, stringsAsFactors = FALSE) {
                                                "decimal", "date_format",
                                                "coord_EPSG", "country_code"))]})
 
-      # Input names for all selectInputs
+      # Input names for all selectInputs (preserving order of data fields)
       select_input_names <- reactive({
-        names(input)[which(names(input) %in% c("from", "to", "date", "weight",
-                                               "id", "coord_x", "coord_y"))]})
+        data_fields <- c("from","to","date","weight","id","coord_x","coord_y")
+        data_fields[which(data_fields %in% names(input))]})
+
+      # Reactive value to store input data in
+      input_data <- reactiveVal(NULL)
 
 # Use config file ---------------------------------------------------------
 
-      # The selected file, if any
       config_file <- reactive({
         # If no file is selected, don't do anything
         validate(need(input$config_file, message = FALSE))
@@ -84,43 +91,81 @@ dataInputServer <- function(id, stringsAsFactors = FALSE) {
       })
 
       observeEvent(config_file(), {
-        # Read in yaml file
-        config_list <- flatten(yaml.load_file(config_file()$datapath))
+        # Read in yaml file (suppressW to avoid flatten outer name warning)
+        config_list <-
+          suppressWarnings(flatten(yaml.load_file(config_file()$datapath)))
 
         # Update inputs
-        sapply(text_input_names()[which(text_input_names() %in% names(config_list))],
+        sapply(text_input_names()
+               [which(text_input_names() %in% names(config_list))],
                function(x){updateTextInput(session, x,
                                            value = config_list[[x]])})
-        sapply(select_input_names()[which(select_input_names() %in% names(config_list))],
+        sapply(select_input_names()
+               [which(select_input_names() %in% names(config_list))],
                function(x){updateSelectInput(session, x,
                                              choices = config_list[[x]],
                                              selected = config_list[[x]])})
       })
 
 
-# Populate column headers -------------------------------------------------
+# Populate column header selectInputs -------------------------------------
 
       observeEvent(input$read_headers, {
 
         dataframe <-
           read_delim(
             input$data_file$datapath,
-            lazy = TRUE,
             delim = input$separator,
             locale = locale(decimal_mark = input$decimal,
-                            #date_format = input$date_format, #if found in inputs, else set to "%AD" !!
-                            encoding = input$encoding),
-            #col_types = cols(.default = col_character()), #only if checking data properties
-            #name_repair = asciify) #copy from movenet
-            show_col_types = FALSE
+                            encoding = input$encoding,
+                            asciify = TRUE), #check that same as in Movenet
+            n_max = 1,
+            show_col_types = FALSE,
+            lazy = TRUE
           )
 
         sapply(select_input_names,
                function(x){
                  updateSelectInput(session, x,
-                                   choices = c("Select appropriate column header",
-                                               colnames(dataframe)))})
+                                   choices =
+                                     c("Select appropriate column header",
+                                       colnames(dataframe)))})
       })
+
+# Extract & reformat data -------------------------------------------------
+
+      observeEvent(input$extract, {
+
+        dataframe <-
+          read_delim(
+            input$data_file$datapath,
+            delim = input$separator,
+            col_select = sapply(select_input_names(), function(x) input[[x]]),
+            locale = locale(decimal_mark = input$decimal,
+                            encoding = input$encoding,
+                            asciify = TRUE), #check that same as in Movenet
+            col_types = cols(.default = col_character()),
+            show_col_types = FALSE,
+            lazy = TRUE) |>
+          type_convert(col_types = cols(date = col_date(),
+                                        weight = col_double(),
+                                        coord_x = col_double(),
+                                        coord_y = col_double()),
+                       locale = locale(decimal_mark = input$decimal,
+                                       date_format =
+                                         ifelse("date_format" %in% names(input),
+                                                            input$date_format,
+                                                            "%AD"),
+                                       encoding = input$encoding))
+
+        #Update the input_data reactiveVal to contain the extracted dataframe
+        input_data(dataframe)
+      })
+
+      observeEvent(input$view_extracted_data, {
+        output$datatable <- renderDataTable({input_data()})
+      })
+
 
     }
   )
