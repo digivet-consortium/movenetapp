8f9fb84d94d7d1c6166c60da00b5ccbefa2d636b
CarlijnB <cbogaardt@gmail.com>
2023-05-04 00:06:14 +0100

    Added/removed several modules, and adapted to new data structure

--- app.R
+++ app.R
@@ -6,22 +6,30 @@ library(yaml) #yaml.load_file
 ui <- fluidPage(
   titlePanel("Movenet"),
   navlistPanel(
-    "Inputs",
-    tabPanel("Movement data",
+    "Input datasets",
+    tabPanel("Input movement data",
              dataInputUI("movement")),
-    tabPanel("Holding data",
-             dataInputUI("holding")),
+    #tabPanel("Holding data",
+    #         dataInputUI("holding")),
+    "Make data non-identifiable",
+    tabPanel("Modify movement dates and/or weights",
+             coarsenDataUI("coarsen")),
+    tabPanel("Modify holding identifiers (pseudonymise)",
+             anonymiseUI("anonymise")),
+    tabPanel("View and/or download datasets",
+             viewDataUI("view_data")),
     "Network analysis",
     tabPanel("Generate networks & calculate measures",
              parallelProcessingInput(),
              createNetworksUI("networks"),
              calculateMeasureUI("overall_measures")),
-    tabPanel("Explore a single network"),
+    tabPanel("Explore a single network",
+             exploreNetworkUI("explore")),
     tabPanel("Compare network measures across jitter/rounding ranges",
              plotMeasureOverGradientUI("maximum reachability")),
     tabPanel("Compare distributions of monthly measures across networks",
              plotMonthlyMeasureViolinplotUI("maximum reachability")),
-    "Modelling",
+    #"Modelling",
     widths = c(3, 9)
   )
 )
@@ -29,10 +37,20 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   movement_data <- dataInputServer("movement")
-  holding_data <- dataInputServer("holding")
-  networks <- createNetworksServer("networks", movement_data, holding_data,
-                                   n_threads = reactive(input$threads))
+  #holding_data <- dataInputServer("holding")
+  modified_movement_data <- coarsenDataServer("coarsen", movement_data)
+  anonymised_movement_data <- anonymiseServer("anonymise", movement_data,
+                                              modified_movement_data)
+  viewDataServer("view_data", movement_data, modified_movement_data,
+                 anonymised_movement_data)
+  nw <- createNetworksServer("networks", movement_data, modified_movement_data,
+                             anonymised_movement_data, #holding_data,
+                             n_threads = reactive(input$threads))
+  networks <- nw$networks
+  monthly_networks <- nw$monthly_networks
+  exploreNetworkServer("explore", networks, n_threads = reactive(input$threads))
   measures <- calculateMeasureServer("overall_measures", networks,
+                                     monthly_networks,
                                      n_threads = reactive(input$threads))
   plotMeasureOverGradientServer("maximum reachability", measures)
   plotMonthlyMeasureViolinplotServer("maximum reachability", measures)
