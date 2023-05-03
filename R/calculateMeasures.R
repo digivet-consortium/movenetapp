baab7ac628eedb91ad07f3a17c605bf7eb063e64
CarlijnB <cbogaardt@gmail.com>
2023-05-04 00:04:43 +0100

    Adapted calculateMeasures module to changed data structure

--- R/calculateMeasures.R
+++ R/calculateMeasures.R
@@ -8,7 +8,7 @@ library(tsna)
 calculateMeasureUI <- function(id) {
   ns <- NS(id) # `NS(id)` returns a namespace function
   tagList(
-    h3("Calculate network measures"),
+    h3("Calculate network measures across all networks"),
     fluidRow(
       column(4,
              h4("Overall network measures"),
@@ -30,25 +30,25 @@ calculateMeasureUI <- function(id) {
 ### Calculate overall network measure - Server logic ###
 ########################################################
 
-calculateMeasureServer <- function(id, networks, n_threads) {
+calculateMeasureServer <- function(id, networks, monthly_networks, n_threads) {
   moduleServer(
     id,
     function(input, output, session) {
 
-      whole_networks <-
-        reactive({list(networks$true, networks$jitter, networks$rounded)})
-      monthly_networks <- reactive({
-        c(networks$true_monthly, networks$jitter_monthly,
-          networks$rounded_monthly) |>
-        setNames(c(names(networks$true_monthly),
-                   paste0("jitter (", names(networks$jitter_monthly), " days)"),
-                   paste0("rounded (", names(networks$rounded_monthly),")")))
-        })
+      # whole_networks <-
+      #   reactive({list(networks$true, networks$jitter, networks$rounded)})
+      # monthly_networks <- reactive({
+      #   c(networks$true_monthly, networks$jitter_monthly,
+      #     networks$rounded_monthly) |>
+      #   setNames(c(names(networks$true_monthly),
+      #              paste0("jitter (", names(networks$jitter_monthly), " days)"),
+      #              paste0("rounded (", names(networks$rounded_monthly),")")))
+      #   })
 
-      n_whole_networks <-
-        reactive({length(unlist(whole_networks(), recursive = FALSE))})
+      n_whole_networks <- reactive({length(networks)})
+      monthly_nw <- reactive({reactiveValuesToList(monthly_networks)})
       n_monthly_networks <-
-        reactive({length(unlist(monthly_networks(), recursive = FALSE))})
+        reactive({length(unlist(monthly_nw(), recursive = FALSE))})
 
       measures <- reactiveValues()
 
@@ -57,44 +57,31 @@ calculateMeasureServer <- function(id, networks, n_threads) {
       observeEvent(input$calc_max_reachability, {
 
         measures$max_reachability <-
-          lapply(seq_along(whole_networks()),
-                 function(x){
-                   max_reach <-
-                     parallel_max_reachabilities(whole_networks()[[x]],
-                                                 n_threads())
-                   updateProgressBar(session, "pb_max_reachability",
-                                     value =
-                                       length(unlist(whole_networks()[1:x],
-                                                     recursive = FALSE)),
-                                     total = n_whole_networks(),
-                                     range_value = c(0, n_whole_networks()))
-                   return(max_reach)
-                 }) |>
-          setNames(c("true","jitter","rounded")
-                   [which(c("true","jitter","rounded") %in% names(networks))])
-
+          parallel_max_reachabilities(networks, n_threads())
+        updateProgressBar(session, "pb_max_reachability",
+                          value = n_whole_networks(),
+                          total = n_whole_networks(),
+                          range_value = c(0, n_whole_networks()))
         })
 
 # Maximum reachability (monthly) ------------------------------------------
 
       observeEvent(input$calc_monthly_max_reachability, {
-
         measures$monthly_max_reachability <-
-          lapply(seq_along(monthly_networks()),
+          lapply(seq_along(monthly_nw()),
                  function(x){
                    max_reach <-
-                     parallel_max_reachabilities(monthly_networks()[[x]],
+                     parallel_max_reachabilities(monthly_nw()[[x]],
                                                  n_threads())
                    updateProgressBar(session, "pb_monthly_max_reachability",
                                      value =
-                                       length(unlist(monthly_networks()[1:x],
+                                       length(unlist(monthly_nw()[1:x],
                                                      recursive = FALSE)),
                                      total = n_monthly_networks(),
                                      range_value = c(0, n_monthly_networks()))
                    return(max_reach)
                  }) |>
-          setNames(names(monthly_networks()))
-
+          setNames(names(monthly_nw()))
         })
 
       #return measures to the main app
