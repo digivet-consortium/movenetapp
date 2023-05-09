5160ddf61cadce2a5d1e4d3e73b4f611cdccf839
CarlijnB <cbogaardt@gmail.com>
2023-05-09 13:06:15 +0100

    Commented-out creation & reachabilities of monthly networks , for workshop.
    Issue: monthly reachabilities don't make sense for rounding units > 1 month -> errors

--- R/calculateMeasures.R
+++ R/calculateMeasures.R
@@ -13,46 +13,37 @@ calculateMeasureUI <- function(id) {
   ns <- NS(id) # `NS(id)` returns a namespace function
   tagList(
     h3("Calculate network measures across all networks"),
-    fluidRow(
-      column(4,
-             h4("Overall network measures"),
+    # fluidRow(
+    #   column(4,
+             # h4("Overall network measures"),
              actionButton(ns("calc_max_reachability"),
-                          "Maximum reachability", width = "100%"),
+                          "Maximum reachability (slow)", width = "100%"),
              progressBar(ns("pb_max_reachability"), value = 0,
-                         display_pct = TRUE)),
-      column(2),
-      column(4,
-             h4("Monthly network measures"),
-             actionButton(ns("calc_monthly_max_reachability"),
-                          "Monthly maximum reachability", width = "100%"),
-             progressBar(ns("pb_monthly_max_reachability"), value = 0,
-                         display_pct = TRUE)))
-    )
+                         display_pct = TRUE))#,
+      # column(2),
+      # column(4,
+      #        h4("Monthly network measures"),
+      #        actionButton(ns("calc_monthly_max_reachability"),
+      #                     "Monthly maximum reachability", width = "100%"),
+      #        progressBar(ns("pb_monthly_max_reachability"), value = 0,
+      #                    display_pct = TRUE)))
+    # )
 }
 
 ########################################################
 ### Calculate overall network measure - Server logic ###
 ########################################################
 
-calculateMeasureServer <- function(id, networks, monthly_networks, n_threads) {
+calculateMeasureServer <- function(id, networks, #monthly_networks,
+                                   n_threads) {
   moduleServer(
     id,
     function(input, output, session) {
 
-      # whole_networks <-
-      #   reactive({list(networks$true, networks$jitter, networks$rounded)})
-      # monthly_networks <- reactive({
-      #   c(networks$true_monthly, networks$jitter_monthly,
-      #     networks$rounded_monthly) |>
-      #   setNames(c(names(networks$true_monthly),
-      #              paste0("jitter (", names(networks$jitter_monthly), " days)"),
-      #              paste0("rounded (", names(networks$rounded_monthly),")")))
-      #   })
-
       n_whole_networks <- reactive({length(networks)})
-      monthly_nw <- reactive({reactiveValuesToList(monthly_networks)})
-      n_monthly_networks <-
-        reactive({length(unlist(monthly_nw(), recursive = FALSE))})
+      #monthly_nw <- reactive({reactiveValuesToList(monthly_networks)})
+      #n_monthly_networks <-
+      #  reactive({length(unlist(monthly_nw(), recursive = FALSE))})
 
       measures <- reactiveValues()
 
@@ -68,25 +59,25 @@ calculateMeasureServer <- function(id, networks, monthly_networks, n_threads) {
                           range_value = c(0, n_whole_networks()))
         })
 
-# Maximum reachability (monthly) ------------------------------------------
-
-      observeEvent(input$calc_monthly_max_reachability, {
-        measures$monthly_max_reachability <-
-          lapply(seq_along(monthly_nw()),
-                 function(x){
-                   max_reach <-
-                     parallel_max_reachabilities(monthly_nw()[[x]],
-                                                 n_threads())
-                   updateProgressBar(session, "pb_monthly_max_reachability",
-                                     value =
-                                       length(unlist(monthly_nw()[1:x],
-                                                     recursive = FALSE)),
-                                     total = n_monthly_networks(),
-                                     range_value = c(0, n_monthly_networks()))
-                   return(max_reach)
-                 }) |>
-          setNames(names(monthly_nw()))
-        })
+# # Maximum reachability (monthly) ------------------------------------------
+#
+#       observeEvent(input$calc_monthly_max_reachability, {
+#         measures$monthly_max_reachability <-
+#           lapply(seq_along(monthly_nw()),
+#                  function(x){
+#                    max_reach <-
+#                      parallel_max_reachabilities(monthly_nw()[[x]],
+#                                                  n_threads())
+#                    updateProgressBar(session, "pb_monthly_max_reachability",
+#                                      value =
+#                                        length(unlist(monthly_nw()[1:x],
+#                                                      recursive = FALSE)),
+#                                      total = n_monthly_networks(),
+#                                      range_value = c(0, n_monthly_networks()))
+#                    return(max_reach)
+#                  }) |>
+#           setNames(names(monthly_nw()))
+#         })
 
       #return measures to the main app
       return(measures)
--- R/createNetworks.R
+++ R/createNetworks.R
@@ -22,35 +22,6 @@
 createNetworksUI <- function(id) {
   ns <- NS(id) # `NS(id)` returns a namespace function
   tagList(
-    # h3("Anonymisation options"),
-    # # Add explanatory blurb
-    # fluidRow(
-    #   column(6,
-    #          h4("Jitter"),
-    #          checkboxGroupInput(ns("jitter_days"),
-    #                             label = "Range of jitter (+/- n days) to apply
-    #                             to dates",
-    #                             choices = list(
-    #                               "4 (~ equivalent to range of 1 week)" = 4,
-    #                               "15 (~ equivalent to range of 1 month)" = 15,
-    #                               "30 (~ equivalent to range of 2 months)" = 30,
-    #                               "46 (~ equivalent to range of 3 months)" = 46,
-    #                               "91 (~ equivalent to range of 6 months)" = 91,
-    #                               "183 (~ equivalent to range of 1 year)" = 183),
-    #                             selected = list(4, 15, 46, 183)),
-    #          numericInput(ns("jitter_simulations"),
-    #                       label =
-    #                         "Number of simulations to perform per range",
-    #                       min = 1,
-    #                       value = 5)),
-    #   column(6,
-    #          h4("Round dates"),
-    #          checkboxGroupInput(ns("rounding_units"),
-    #                             label = "Unit to round dates down to",
-    #                             choices = list("week", "month", "2 months",
-    #                                            "3 months", "6 months", "year"),
-    #                             selected =
-    #                               list("week","month","3 months","year")))),
     h3("Create networks from datasets"),
     selectInput(ns("datasets"), "Select dataset(s) to turn into networks",
                 choices = NULL, multiple = TRUE),
@@ -115,91 +86,29 @@ createNetworksServer <- function(id, movement_data, modified_movement_data,
           lapply(seq_along(selected_datasets()), function(x) {
             net <- movedata2networkDynamic(selected_datasets()[[x]])
             updateProgressBar(session, "create_networks_pb", value = x,
-                              total = counts$n_allnetworks,
-                              range_value = c(0, counts$n_allnetworks))
+                              #total = counts$n_allnetworks,  #with monthly nw
+                              total = counts$n_networks,  #w/o monthly nw
+                              #range_value = c(0, counts$n_allnetworks))  #with monthly nw
+                              range_value = c(0, counts$n_networks))  #w/o monthly nw
             return(net)}) |>
           setNames(names(selected_datasets()))
 
         lapply(seq_along(nw), function(x){networks[[names(nw[x])]] <- nw[[x]]})
 
-        #Creating monthly subnetworks
-        monthly_nw <- extract_periodic_subnetworks(nw, n_threads(),
-                                                   counts$months_in_data)
-        updateProgressBar(session, "create_networks_pb",
-                          value = counts$n_allnetworks,
-                          total = counts$n_allnetworks,
-                          range_value = c(0, counts$n_allnetworks))
 
-        lapply(seq_along(monthly_nw), function(x){
-          monthly_networks[[names(monthly_nw[x])]] <- monthly_nw[[x]]})
-
-        })
-
-        # #Create true network
-        # networks$true <- list(true = movedata2networkDynamic(anonymised_data))
-        # updateProgressBar(session, "create_networks_pb", value = 1,
-        #                   total = counts$n_allnetworks,
-        #                   range_value = c(0, counts$n_allnetworks))
-        #
-        # #Apply jitter
-        # jitter_days_reps <- rep(input$jitter_days, input$jitter_simulations)
-        # networks$jitter <-
-        #   lapply(seq_along(jitter_days_reps), function(x){
-        #     nw <- simplified_coarsen_date(anonymised_data,
-        #                                   rounding_unit = FALSE,
-        #                                   jitter = jitter_days_reps[x]) |>
-        #       movedata2networkDynamic()
-        #     updateProgressBar(session, "create_networks_pb", value = 1 + x,
-        #                       total = counts$n_allnetworks,
-        #                       range_value = c(0, counts$n_allnetworks))
-        #     return(nw)}) |>
-        #   setNames(rep(input$jitter_days, input$jitter_simulations))
-        #
-        # #Apply rounding
-        # week_start <- wday(min(anonymised_data[['date']]))
-        # networks$rounded <-
-        #   lapply(seq_along(input$rounding_units), function(x){
-        #     nw <-
-        #       simplified_coarsen_date(anonymised_data, jitter = FALSE,
-        #                               rounding_unit = input$rounding_units[x],
-        #                               week_start = week_start) |>
-        #       movedata2networkDynamic()
-        #     updateProgressBar(session, "create_networks_pb",
-        #                       value = 1 + counts$n_jitter_networks + x,
-        #                       total = counts$n_allnetworks,
-        #                       range_value = c(0, counts$n_allnetworks))
-        #     return(nw)}) |>
-        #   setNames(input$rounding_units)
-
-        # #Create monthly networks
-        # networks$true_monthly <-
-        #   extract_periodic_subnetworks(networks$true, n_threads(),
-        #                                counts$months_in_data)
-        # updateProgressBar(session, "create_networks_pb",
-        #                   value =
-        #                     counts$n_networks + length(counts$months_in_data),
-        #                   total = counts$n_allnetworks,
-        #                   range_value = c(0, counts$n_allnetworks))
-        #
-        # networks$jitter_monthly <-
-        #   extract_periodic_subnetworks(networks$jitter, n_threads(),
-        #                                counts$months_in_data)
-        # updateProgressBar(session, "create_networks_pb",
-        #                   value =
-        #                     counts$n_networks + (counts$n_jitter_networks + 1)*
-        #                     length(counts$months_in_data),
-        #                   total = counts$n_allnetworks,
-        #                   range_value = c(0, counts$n_allnetworks))
-        #
-        # networks$rounded_monthly <-
-        #   extract_periodic_subnetworks(networks$rounded, n_threads(),
-        #                                counts$months_in_data)
+        # #Creating monthly subnetworks  - WORKS BUT TAKEN OUT FOR WORKSHOP
+        # monthly_nw <- extract_periodic_subnetworks(nw, n_threads(),
+        #                                            counts$months_in_data)
         # updateProgressBar(session, "create_networks_pb",
         #                   value = counts$n_allnetworks,
         #                   total = counts$n_allnetworks,
         #                   range_value = c(0, counts$n_allnetworks))
-      # })
+        #
+        # lapply(seq_along(monthly_nw), function(x){
+        #   monthly_networks[[names(monthly_nw[x])]] <- monthly_nw[[x]]})
 
-      return(list(networks = networks, monthly_networks = monthly_networks))
+        })
+
+      return(list(networks = networks))#, monthly_networks = monthly_networks))
 
 })}
