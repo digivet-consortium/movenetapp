4c47189101b18fc0eca439928f4e02d6ef650233
CarlijnB <cbogaardt@gmail.com>
2023-05-12 10:30:29 +0100

    Reduced jitter range boxes to 3, commented-out simulations
    to speed up network generation

--- R/coarsenData.R
+++ R/coarsenData.R
@@ -14,9 +14,9 @@ coarsenDataUI <- function(id) {
       (down to the first day of a selected time unit). For each jitter range or
       rounding unit provided, a new dataset will be created. You can view these
       datasets by selecting 'View and/or download datasets' in the sidebar menu."),
-    p("As jittering dates contains an element of randomisation, you can choose to
-      perform multiple simulations per range, resulting in the generation of multiple
-      equivalent datasets."),
+    # p("As jittering dates contains an element of randomisation, you can choose to
+    #   perform multiple simulations per range, resulting in the generation of multiple
+    #   equivalent datasets."),
     # selectInput(ns("dataset"),
     #             "Dataset to modify movement dates for",
     #             choices = NULL),
@@ -26,19 +26,17 @@ coarsenDataUI <- function(id) {
              tags$b("Range(s) of jitter (+/- n days) to apply to dates"),
              numericInput(ns("jitter_d_1"), label = NULL, min=0, value = 3),
              numericInput(ns("jitter_d_2"), label = NULL, min=0, value = NULL),
-             numericInput(ns("jitter_d_3"), label = NULL, min=0, value = NULL),
-             numericInput(ns("jitter_d_4"), label = NULL, min=0, value = NULL),
-             numericInput(ns("jitter_d_5"), label = NULL, min=0, value = NULL),
-             numericInput(ns("jitter_d_sims"),
-                          "Number of simulations to perform per range",
-                          min = 1, value = 3)),
+             numericInput(ns("jitter_d_3"), label = NULL, min=0, value = NULL)),
+             # numericInput(ns("jitter_d_sims"),
+             #              "Number of simulations to perform per range",
+             #              min = 1, value = 3)),
       column(6,
              h4("Round dates"),
              #textInput(ns("rounding_units"), "Time unit to round dates down to"), #equivalent to function, may raise errors due to unacceptable time units
              selectInput(ns("rounding_units_d"), "Time unit(s) to round dates down to",
-                         choices = list("No rounding" = "", "week", "month",
-                                        "2 months", "3 months", "4 months",
-                                        "6 months", "year"), #more restrictive than function in package, but sensible options
+                         choices = list("No rounding" = "", "week", "month", "3 months"),
+                                        #"2 months", "3 months", "4 months",
+                                        #"6 months", "year"), #more restrictive than function in package, but sensible options
                          multiple = TRUE),
              tags$b("Starting day of the week (if rounding to week)"),
              selectInput(ns("week_start"), label = NULL,
@@ -48,8 +46,8 @@ coarsenDataUI <- function(id) {
                                         "weekday of first movement in dataset" =
                                         "from_data"), #Sensible options but different from package
                          selected = 1), #in package the default is lubridate setting (Sunday by default) but for European countries I think Monday makes more sense
-             tags$b("Aggregate/summarise data for repeated movements over the selected time unit?"),
-             checkboxInput(ns("sum_weight"), "Aggregate movement weights",
+             #tags$b("Aggregate/summarise data for repeated movements over the selected time unit?"),
+             checkboxInput(ns("sum_weight"), "Aggregate weights for repeated movements over the selected time unit",
                            value = TRUE),
              #p("Additional or alternative summary function(s)"),
              #textInput(ns("alt_summary_function"), label = NULL),
@@ -65,8 +63,6 @@ coarsenDataUI <- function(id) {
     #          numericInput(ns("jitter_w_1"), label = NULL, min=0, value = 5),
     #          numericInput(ns("jitter_w_2"), label = NULL, min=0, value = NULL),
     #          numericInput(ns("jitter_w_3"), label = NULL, min=0, value = NULL),
-    #          numericInput(ns("jitter_w_4"), label = NULL, min=0, value = NULL),
-    #          numericInput(ns("jitter_w_5"), label = NULL, min=0, value = NULL),
     #          numericInput(ns("jitter_w_sims"),
     #                       "Number of simulations to perform per range",
     #                       min = 0, value = 3)),
@@ -75,10 +71,8 @@ coarsenDataUI <- function(id) {
     #          tags$b("Number to round weights to"),
     #          numericInput(ns("rounding_w_1"), label = NULL, min=0, value = NULL),
     #          numericInput(ns("rounding_w_2"), label = NULL, min=0, value = NULL),
-    #          numericInput(ns("rounding_w_3"), label = NULL, min=0, value = NULL),
-    #          numericInput(ns("rounding_w_4"), label = NULL, min=0, value = NULL),
-    #          numericInput(ns("rounding_w_5"), label = NULL, min=0, value = NULL))),
-    h3("Parallel processing"),
+    #          numericInput(ns("rounding_w_3), label = NULL, min=0, value = NULL))),
+    h4("Parallel processing"),
     numericInput(ns("n_threads"), label = "Number of threads available",
                  min = 1, value = 4),
     actionButton(ns("modify_data"), "Modify movement data", width = "100%"),
@@ -120,10 +114,9 @@ coarsenDataServer <- function(id, movement_data){
 
 
       observeEvent(input$modify_data, {
-
-        jitter_d_set <- rep(c(input$jitter_d_1, input$jitter_d_2,
-                              input$jitter_d_3, input$jitter_d_4,
-                              input$jitter_d_5), input$jitter_d_sims)
+        jitter_d_set <- c(input$jitter_d_1, input$jitter_d_2, input$jitter_d_3)
+        # jitter_d_set <- rep(c(input$jitter_d_1, input$jitter_d_2,
+        #                       input$jitter_d_3), input$jitter_d_sims)
         jitter_d_set <- jitter_d_set[which(jitter_d_set != 0 & !is.na(jitter_d_set))]
         week_start <- ifelse(input$week_start == "from_data",
                              wday(min(movement_data$original[[3]])),
@@ -143,13 +136,11 @@ coarsenDataServer <- function(id, movement_data){
                            total = 1, range_value = c(0, 1))
 
         # jitter_w_set <- rep(c(input$jitter_w_1, input$jitter_w_2,
-        #                       input$jitter_w_3, input$jitter_w_4,
-        #                       input$jitter_w_5), input$jitter_w_sims)
+        #                       input$jitter_w_3), input$jitter_w_sims)
         # jitter_w_set <- jitter_w_set[which(jitter_w_set != 0 & !is.na(jitter_w_set))]
         #
         # rounding_w_set <- c(input$rounding_w_1, input$rounding_w_2,
-        #                     input$rounding_w_3, input$rounding_w_4,
-        #                     input$rounding_w_5)
+        #                     input$rounding_w_3)
         # rounding_w_set <- rounding_w_set[which(rounding_w_set != 0 & !is.na(rounding_w_set))]
         #
         # ## N.B. The below doesnt work - in part because function has not actually
