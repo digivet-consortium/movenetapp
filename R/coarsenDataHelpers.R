77028adadaee0e7fefb2daa29030d1ac9ac86112
CarlijnB <cbogaardt@gmail.com>
2023-10-07 22:37:27 +0100

    deleted simpl_coarsen_date & updated par_coarsen_dates & added par_coarsen_weights

--- R/coarsenDataHelpers.R
+++ R/coarsenDataHelpers.R
@@ -1,247 +1,111 @@
-#library(lubridate)
-#library(dplyr)
-#library(rlang)
-
-#' @import lubridate
-#' @import dplyr
-#' @importFrom rlang .data
-#'
-
-simplified_coarsen_date <- function(data, jitter,
-                                    rounding_unit,
-                                    week_start = getOption("lubridate.week.start", 7),
-                                    sum_weight = TRUE, ...){
-
-  # Removed all argument checks and config checks from the movenet version;
-  # changed all references to movenetenv variables -> from/to/date/weight , as
-  # in webapp the data cols were renamed
-
-  dates <- data[["date"]]
-
-  #####################
-  ### Adding jitter ###
-  #####################
-
-  if (!isFALSE(jitter) | jitter != 0){
-
-    jitter <- as.integer(jitter)
-
-    replacement_data <-
-      dates +
-      sample(c(-jitter:jitter),
-             length(dates),
-             replace = TRUE)
-
-    while (any(replacement_data < min(dates) | replacement_data > max(dates))){
-
-      #resampling for dates beyond boundaries
-      replacement_data[which(replacement_data < min(dates) |
-                               replacement_data > max(dates))] <-
-        dates[which(replacement_data < min(dates) |
-                      replacement_data > max(dates))] +
-        sample(c(-jitter:jitter),
-               sum(any(replacement_data < min(dates) |
-                         replacement_data > max(dates))),
-               replace = TRUE)
-
-    }
-
-    data["date"] <- replacement_data
-  }
-
-  # Currently adds uniform ints between -jitter and +jitter , including 0 !
-
-
-  ###########################
-  ### Rounding down dates ###
-  ###########################
-
-  if (!isFALSE(rounding_unit)){
-    # round down each date to the first date of the rounding_unit (e.g. month)
-    rounded_data <-
-      data %>%
-      mutate(date =
-             floor_date(.data$date,
-                        unit = rounding_unit,
-                        week_start = week_start))
-
-    #what to do to allow user to coarsen other date fields?
-    #  build in column argument like for coarsen_weight?
-
-    #Alternatives:
-    # format_ISO8601(date_object, precision) from lubridate ->
-    #      character vector matching ISO format to certain precision
-    # format(date_object, format_string) from base R ->
-    #      character vector matching format_string (e.g %Y-%m)
-    # as.yearmon(date_object) from zoo ->
-    #      yearmon object
-
-    # Allow for aggregation over consistent periods of x number of days?
-    # Lentz uses 1d, 7d, 14d, 28d (monthly), 84d (quarterly)
-
-
-    ###########################
-    ### Aggregating by date ###   (only if !isFALSE(rounding_unit))
-    ###########################
-
-    if (isTRUE(sum_weight)){
-
-      aggregated_data <-
-        rounded_data %>%
-        group_by(.data$from, .data$to, .data$date) %>%
-        summarise(summed_weight = sum(.data$weight),  ...) %>%
-        ungroup() %>%
-        rename(weight = .data$summed_weight)
-      #using "summed_weight" and then renaming to the data-specific weight
-      #variable, to avoid problems with additional weight summarising
-      #functions. If the original name is kept, any additional functions are
-      #performed on the summed weights rather than individual weights
-
-      return(aggregated_data)
-
-    } else if(...length() == 0){
-
-      return(rounded_data)
-
-    } else {
-
-      aggregated_data <-
-        rounded_data %>%
-        group_by(.data$from, .data$to, .data$date) %>%
-        summarise(...) %>%
-        ungroup()
-
-      return(aggregated_data)
-    }
-
-    ###############################################
-    ### Returning jittered data, if no rounding ###
-    ###############################################
-
-  } else return(data)
-}
-
-parallel_coarsen_date <- function(data, n_threads, jitter_set, rounding_set,
-                                  week_start = getOption("lubridate.week.start", 7),
-                                  sum_weight = TRUE, ...){
-
-  jitter_reps <- c(jitter_set, rep(FALSE, length(rounding_set)))
-  rounding_reps <- c(rep(list(FALSE),length(jitter_set)), rounding_set)
+parallel_coarsen_dates <- function(data, n_threads, jitter_set, rounding_set,
+                                   week_start, sum_weight){
 
   cl <- makeCluster(n_threads)
   on.exit(stopCluster(cl))
 
-  clusterExport(cl, c("data", "jitter_reps", "rounding_reps", "week_start",
-                      "sum_weight", "floor_date", ":="), envir = environment())
+  clusterExport(cl, c("data", "jitter_set", "rounding_set", "week_start",
+                      "sum_weight"), envir = environment())
+  clusterExport(cl, "movenetenv", envir = parent.env(movenetenv))
+
   clusterEvalQ(cl, {
     library("dplyr")
+    library("movenet")
+    for(n in ls(movenetenv, all.names = TRUE)){ #copies movenetenv to movenet:::movenetenv
+      assign(n, get(n, movenetenv), movenet:::movenetenv)
+    }
     })
 
-  modified_datasets <-
-    parLapply(cl = cl, seq_along(jitter_reps),
-              function(x){simplified_coarsen_date(
-                data = data,
-                jitter = jitter_reps[[x]],
-                rounding_unit = rounding_reps[[x]],
-                week_start = week_start,
-                sum_weight = sum_weight,
-                ...)})
+  jittered_datasets <- NULL
+  rounded_datasets <- NULL
+  dataname <- names(data)
 
-  jitter_names <-
-    if(length(jitter_set) == 0){NULL
-      } else {paste0("Jittered_", jitter_set,"days")}
+  if(length(jitter_set) > 0){
+    jittered_datasets <-
+      parLapply(cl = cl, seq_along(jitter_set),
+                function(x){jitter_dates(data = data[[names(data)]],
+                                         range = jitter_set[[x]])})
 
-  rounded_names <-
-    if(length(rounding_set) == 0){NULL
-      } else {paste0("Rounded_", gsub(" ", "", rounding_set), "WS", week_start)}
-  #Names miss reference to summary functions - would get too complicated
-
-  names(modified_datasets) <-
-    make.unique(paste0("Dates", c(jitter_names, rounded_names)), sep="_")
-
-  return(modified_datasets)
-}
-
-#' @importFrom plyr round_any
-#' @importFrom purrr has_element
-#' @importFrom stats runif
-simplified_coarsen_weight <- function(data, column, jitter, round){
-
-  #####################
-  ### Adding jitter ###
-  #####################
-
-  if (!isFALSE(jitter) | jitter != 0){
-
-    replacement_data <-
-      data[[column]] +
-      runif(length(data[[column]]), -jitter, +jitter)
-
-    while (any(replacement_data <= 0)){ #resampling if result of jitter is <= 0
-
-      replacement_data[which(replacement_data <= 0)] <-
-        data[[column]][which(replacement_data <= 0)] +
-        runif(sum(replacement_data <= 0), -jitter, +jitter)
-
+    if(dataname == "original"){
+      names(jittered_datasets) <-
+        make.unique(paste0("DatesJittered_", jitter_set, "d"), sep="_")
+    } else {
+      names(jittered_datasets) <-
+        make.unique(paste0(dataname, "_DatesJittered_", jitter_set, "d"), sep="_")
     }
-
-    data[column] <- replacement_data
   }
 
-  # Most suitable distribution? Currently uniform, but can change
-
-
-  ################
-  ### Rounding ###
-  ################
-
-  if (!isFALSE(round) | round != 0){
-
-    data[column] <- round_any(data[[column]], accuracy = round)
-
-    data[column][which(data[column] < round),] <- round #set round as minimum
-
-    #is it more efficient to filter first & only round if data[column] > round,
-    #or to just round everything?
+  if(length(rounding_set) > 0){
+    rounded_datasets <-
+      parLapply(cl = cl, seq_along(rounding_set),
+                function(x){round_dates(data = data[[names(data)]],
+                                        unit = rounding_set[[x]],
+                                        week_start = week_start,
+                                        sum_weight = sum_weight)})
+    summed <- switch(sum_weight, "aggr")
+    if(dataname == "original"){
+      names(rounded_datasets) <-
+        paste0("DatesRounded_", gsub(" ", "", rounding_set), "WS", week_start,
+               summed)
+    } else {
+      names(rounded_datasets) <-
+        paste0(dataname, "_DatesRounded_", gsub(" ", "", rounding_set), "WS", week_start,
+               summed)
+    }
   }
 
-  return(data)
+  return(c(jittered_datasets,rounded_datasets))
 }
 
-## N.B. The below doesnt work - in part because function has not actually
-##      been parallelised for multiple datasets!!
-parallel_coarsen_weight <- function(data, n_threads, jitter_set, rounding_set){
-
-  jitter_reps <- c(jitter_set, rep(FALSE, length(rounding_set)))
-  rounding_reps <- c(rep(FALSE, length(jitter_set)), rounding_set)
+parallel_coarsen_weights <- function(data, n_threads, jitter_set, rounding_set,
+                                     column){
 
   cl <- makeCluster(n_threads)
   on.exit(stopCluster(cl))
 
-  clusterExport(cl, c("data", "jitter_reps", "rounding_reps"),
+  clusterExport(cl, c("data", "jitter_set", "rounding_set"),
                 envir = environment())
+  clusterExport(cl, "movenetenv", envir = parent.env(movenetenv))
+
   clusterEvalQ(cl, {
     library("dplyr")
+    library("movenet")
+    for(n in ls(movenetenv, all.names = TRUE)){ #copies movenetenv to movenet:::movenetenv
+      assign(n, get(n, movenetenv), movenet:::movenetenv)
+    }
   })
 
-  modified_datasets <-
-    parLapply(cl = cl, seq_along(jitter_reps),
-              function(x){simplified_coarsen_weight(
-                data = data,
-                column = "weight",
-                jitter = jitter_reps[[x]],
-                round = rounding_reps[[x]])})
+  jittered_datasets <- NULL
+  rounded_datasets <- NULL
+  dataname <- names(data)
 
-  jitter_names <-
-    if(length(jitter_set) == 0){NULL
-    } else {paste0("Jittered_", jitter_set)}
+  if(length(jitter_set) > 0){
+    jittered_datasets <-
+      parLapply(cl = cl, seq_along(jitter_set),
+                function(x){jitter_weights(data = data[[names(data)]],
+                                           range = jitter_set[[x]],
+                                           column = column)})
+    if(dataname == "original"){
+      names(jittered_datasets) <-
+        make.unique(paste0("WeightsJittered_", jitter_set), sep="_")
+    } else {
+      names(jittered_datasets) <-
+        make.unique(paste0(dataname, "_WeightsJittered_", jitter_set), sep="_")
+    }
+  }
 
-  rounded_names <-
-    if(length(rounding_set) == 0){NULL
-    } else {paste0("Rounded_", rounding_set)}
+  if(length(rounding_set) > 0){
+    rounded_datasets <-
+      parLapply(cl = cl, seq_along(rounding_set),
+                function(x){round_weights(data = data[[names(data)]],
+                                          unit = rounding_set[[x]],
+                                          column = column)})
+    if(dataname == "original"){
+      names(rounded_datasets) <- paste0("WeightsRounded_", rounding_set)
+    } else {
+      names(rounded_datasets) <- paste0(dataname, "_WeightsRounded_", rounding_set)
+    }
+  }
 
-  names(modified_datasets) <-
-    make.unique(paste0("Weights", c(jitter_names, rounded_names)), sep="_")
-
-  return(modified_datasets)
+  return(c(jittered_datasets, rounded_datasets))
 }
